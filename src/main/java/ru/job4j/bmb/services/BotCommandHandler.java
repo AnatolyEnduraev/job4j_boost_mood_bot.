c17ae560088146bc7abb2e29e00c35a5925a694e
Анатолий <eao73@mail.ru>
2026-03-25 21:06:25 +0300

    Aware интерфейсы [#505301]

--- src/main/java/ru/job4j/bmb/services/BotCommandHandler.java
+++ src/main/java/ru/job4j/bmb/services/BotCommandHandler.java
@@ -1,10 +1,16 @@
 package ru.job4j.bmb.services;
 
+import org.springframework.beans.factory.BeanNameAware;
 import org.springframework.stereotype.Service;
 import ru.job4j.bmb.content.Content;
 
   @Service
-public class BotCommandHandler {
+public class BotCommandHandler implements BeanNameAware {
+      @Override
+      public void setBeanName(String name) {
+          System.out.println("Имя бина в контексте Spring: " + name);
+      }
+
     void receive(Content content) {
         System.out.println(content);
     }
--- src/main/java/ru/job4j/bmb/services/TelegramBotService.java
+++ src/main/java/ru/job4j/bmb/services/TelegramBotService.java
@@ -1,14 +1,20 @@
 package ru.job4j.bmb.services;
 
+import org.springframework.beans.factory.BeanNameAware;
 import org.springframework.stereotype.Service;
 import ru.job4j.bmb.content.Content;
 
 @Service
-public class TelegramBotService {
+public class TelegramBotService implements BeanNameAware {
     private final BotCommandHandler handler;
 
     public TelegramBotService(BotCommandHandler handler) {
         this.handler = handler;
+    }
+
+    @Override
+    public void setBeanName(String name) {
+        System.out.println("Имя бина в контексте Spring: " + name);
     }
 
     public void receive(Content content) {
